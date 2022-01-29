dd1fcf28fa97096571a4cafa60d7a963a886fdb8
Karol Giron <kbenites4518@gmail.com>
2022-01-29 17:12:17 -0600

    Ejercicio 3

---
+++ Ejercicios/Ejercicio 3/Variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int numeroEntero;
+
+    numeroEntero = 15;
+
+    cout << numeroEntero << endl;
+
+    numeroEntero = 18;
+
+    cout << numeroEntero << endl;
+
+
+    return 0;
+}
