90d67ddd74855cd22db061493f61372375d858ce
Karol Giron <kbenites4518@gmail.com>
2022-01-29 15:27:42 -0600

    Agregando un espacio

--- Ejercicios/Ejercicio 2/mensajes.cpp
+++ Ejercicios/Ejercicio 2/mensajes.cpp
@@ -10,7 +10,7 @@ int main(int argc, char const *argv[])
     cout << "Ingrese su nombre : ";
     
     cout << endl;
-    cout << "Hola, mi nombre es " <<nombre;
+    cout << "Hola, mi nombre es " << nombre;
     cout << endl;
 
 
