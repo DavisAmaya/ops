a7917de34edb88117dc0bb07b13fb4e80daa84d3
Karol Giron <kbenites4518@gmail.com>
2022-02-05 18:46:05 -0600

    editando margen

--- Ejercicios/Ejercicio13 - Factura exenta/main.cpp
+++ Ejercicios/Ejercicio13 - Factura exenta/main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char** argv) {
 	}	
     else
     {
-       calculoImpuesto = (subtotal - calculoDescuento) * 0.15;
+        calculoImpuesto = (subtotal - calculoDescuento) * 0.15;
     }
 
     cout << endl; 
