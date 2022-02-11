f9e6eefb3ad4ef5f177401f2bb1736d42725e4f4
Karol Giron <kbenites4518@gmail.com>
2022-02-11 15:59:40 -0600

    Tarea4 Agg Ejercicio25

---
+++ Ejercicios/Ejercicio25 -  Suma de Pares e impares y Total/Tarea4 de suma total de pares e impares.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int pares = 0;
+    int impares = 0;
+    int sumaTotal = 0;
+
+    cout << endl;
+
+    for (int i = 1; i <= 10; i++)
+    {
+        if (i % 2 == 0)
+        {
+            pares = pares + i;
+        }
+        else
+        {
+            impares += i;
+        }
+
+        sumaTotal = pares + impares;
+
+        
+        cout << i << "  ";
+    }
+
+    cout << endl;
+    cout << endl;
+    cout << "Total pares: " << pares << endl;
+    cout << "Total impares: " << impares << endl;
+    cout << "Suma total de pares + impares: " << sumaTotal;
+
+
+    return 0;
+}
