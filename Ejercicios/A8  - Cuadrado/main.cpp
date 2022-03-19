eb3a85ea71d77354dfd6b4e6c4d852e22582a952
Karol Giron <kbenites4518@gmail.com>
2022-03-19 13:54:56 -0600

    Agregando A8

---
+++ Ejercicios/A8  - Cuadrado/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    double numero1 = 5;
+    double numero2 = 0;
+    
+    //Funcion sqrt que calcula e imprime la raiz cudrada de un numero dado
+
+    numero2 = sqrt (numero1);
+
+    cout << "El numero  " << numero1 << "  tiene como raiz cuadrada: " << numero2 << endl;
+    
+    return 0;
+}
