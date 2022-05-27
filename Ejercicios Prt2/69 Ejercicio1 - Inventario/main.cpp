28683329532e17f9ded6765f4590a6d4544b347a
Karol Giron <kbenites4518@gmail.com>
2022-05-27 17:59:23 -0600

    Modificando case 1

--- Ejercicios Prt2/69 Ejercicio1 - Inventario/main.cpp
+++ Ejercicios Prt2/69 Ejercicio1 - Inventario/main.cpp
@@ -10,6 +10,19 @@ string productos [5][2] = {
     {"005", "HeadSet"},
 };
 
+void listarProductos ()
+{
+    system ("cls"); 
+    cout << endl;
+    cout << "Listado de productos" << endl;
+    cout << "*********************" << endl;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << productos [i][0] << "   " << productos [i][1] << endl;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     int opcion = 0;
@@ -35,16 +48,10 @@ int main(int argc, char const *argv[])
 
         switch (opcion) 
         {
-        case 1:{
-            cout << endl;
-            cout << "Listado de producto" << endl;
-            cout << "********************" << endl;
-
-            for (int i = 0; i < 5; i++)
-            {
-                cout << productos [i][0] << " " << productos [i][1] << endl;
-            }
-        }
+        case 1:
+            listarProductos ();
+            break;
+        
         case 2:
             cout << "Escogiste 2  ";
             break;
