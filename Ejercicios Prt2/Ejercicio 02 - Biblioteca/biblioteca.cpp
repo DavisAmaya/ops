f70d6f9e8efb44a809a09f99d0e4278f0d09ad54
Karol Giron <kbenites4518@gmail.com>
2022-05-28 14:51:07 -0600

    Agg ejercicio2 Biblioteca

---
+++ Ejercicios Prt2/Ejercicio 02 - Biblioteca/biblioteca.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+string libros [2][2];
+
+void cargarLibros (){
+    libros [0][0] = "Algoritmos";
+    libros [0][1] = "Algoritmos y programacion (Guia para docentes)";
+
+    libros [1][0] = "Base de datos";
+    libros [1][1] = "Apuntes de base de datos 1";
+}
+
+
+int main(int argc, char const *argv[])
+{
+    cargarLibros ();
+
+    for (int i = 0; i < 2; i++)
+    {
+        cout << libros [i][1]; 
+    }
+    
+
+    return 0;
+}
